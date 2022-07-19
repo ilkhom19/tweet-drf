7992935c1b7488cf2d069e23011eae2701050fca
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-07-19 09:33:05 +0600

    added db

--- tweetdrf/settings.py
+++ tweetdrf/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'tweetdrf.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tweetproject',
+        'USER': 'tweetuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+
 }
 
 
