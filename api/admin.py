0d8213d903cc42215da49a1d3bfc02108d619319
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-07-20 09:31:02 +0600

    added filters

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Author, Tweet
 
-admin.site.register(Author)
-admin.site.register(Tweet)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name','username','user_id']
+    pass
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = [
+        'tweet',
+        'date',
+        'time',
+        'user_id',
+        'replies_count',
+        'retweets_count',
+        'likes_count',
+        'link',
+        'retweet'
+        ]
+    list_filter = ('user_id')
+    
+    pass
+
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Tweet, TweetAdmin)
